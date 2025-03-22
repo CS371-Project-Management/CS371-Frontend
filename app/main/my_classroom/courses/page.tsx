d6f49e236fe005bc5e32182b55dad34a1c19cdb7
Aquarat <aquarat.t@ku.th>
2025-03-22 20:17:47 +0700

    fix_folder_app_user->main

--- app/main/my_classroom/courses/page.tsx
+++ app/main/my_classroom/courses/page.tsx
@@ -11,35 +11,35 @@ const courses = [
         description: 'เรียนออกแบบ UX/UI ตั้งแต่พื้นฐานจนถึงขั้นสูง',
         image: '/images/image.jpg',
         icon: <Palette size={24} />,
-        path: '/user/my_classroom/courses/lessons'
+        path: '/main/my_classroom/courses/lessons'
     },
     {
         title: 'Data Analytics with Python',
         description: 'วิเคราะห์ข้อมูลด้วย Python และเครื่องมือต่าง ๆ',
         image: '/images/image.jpg',
         icon: <Database size={24} />,
-        path: '/user/my_classroom/courses/lessons'
+        path: '/main/my_classroom/courses/lessons'
     },
     {
         title: 'Fundamental Web Dev',
         description: 'HTML5 และ CSS3 สำหรับมือใหม่',
         image: '/images/image.jpg',
         icon: <Code size={24} />,
-        path: '/user/my_classroom/courses/lessons'
+        path: '/main/my_classroom/courses/lessons'
     },
     {
         title: 'Introduction to JavaScript',
         description: 'พื้นฐานการเขียนโปรแกรมด้วย JavaScript',
         image: '/images/image.jpg',
         icon: <Code size={24} />,
-        path: '/user/my_classroom/courses/lessons'
+        path: '/main/my_classroom/courses/lessons'
     },
     {
         title: 'Web App Development with Node.js',
         description: 'เรียนรู้ Node.js และ Express',
         image: '/images/image.jpg',
         icon: <BookOpen size={24} />,
-        path: '/user/my_classroom/courses/lessons'
+        path: '/main/my_classroom/courses/lessons'
     },
 ];
 
@@ -48,8 +48,13 @@ export default function CoursePage() {
         <div className="min-h-screen bg-white">
             <NavbarClassroom></NavbarClassroom>   
 
-            <div className='ml-20 mt-20 mb-15 text-3xl font-bold'>
-                All Courses
+            <div className='flex flex-between ml-20 mt-20 mb-15 text-3xl font-bold'>
+                <h2 className="text-2xl font-bold mb-15">My Created Classroom</h2>
+
+                <button className="bg-gray-800 p-5 h-fit rounded-xl text-white shadow-2xl"
+                    onClick={() => {}}>
+                    Create New Classroom
+                </button>
             </div>
 
             <div className="flex flex-wrap gap-6 ml-25">
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,10 +7,10 @@ import ModalClassroomPin from './modals/ClassroomPin';
 import Link from 'next/link';
 
 const menuItems = [
-    { name: 'Home', icon: Home, path: '/user/home' },
+    { name: 'Home', icon: Home, path: '/main/home' },
     { name: 'Join Classroom', icon: Plus, path: '#' },
-    { name: 'My Classroom', icon: Book, path: '/user/my_classroom' },
-    { name: 'My Created Classroom', icon: Pencil, path: '/user/my_created_classroom' },
+    { name: 'My Classroom', icon: Book, path: '/main/my_classroom' },
+    { name: 'My Created Classroom', icon: Pencil, path: '/main/my_created_classroom' },
 ];
 
 export default function Sidebar() {
