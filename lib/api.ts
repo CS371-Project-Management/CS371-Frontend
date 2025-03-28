45552f3e91ec66b98d707a516003738c495ba962
Watcharaporn Puvanasuksuntorn <pooh2pin@gmail.com>
2025-03-28 20:20:45 +0700

    fix bug

--- lib/api.ts
+++ lib/api.ts
@@ -3,22 +3,14 @@
 
 // const API_BASE_URL = 'http://localhost:8080/api1';
 
-<<<<<<< HEAD
-const axiosInstance = axios.create({
-    baseURL: API_BASE_URL,
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer' + localStorage.getItem('token')
-    },
-  });
-=======
+
 // const axiosInstance = axios.create({
 //     baseURL: API_BASE_URL,
 //     headers: {
 //       'Content-Type': 'application/json',
 //     },
 //   });
->>>>>>> 52bab1cd67f609dc77568dd5fe7785242e1f3a7b
+
 
 // function addAuthToken(
 //     config: InternalAxiosRequestConfig
